f7100833e3836032bb4ef850ef14aaf96b7c3424
Adrian H <adrian.harenczyk@gmail.com>
2022-04-06 07:50:15 +0200

    Add tournament winner algorithm.

--- src/main/java/base/Main.java
+++ src/main/java/base/Main.java
@@ -1,13 +1,16 @@
 package base;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import static java.lang.Math.abs;
 
 public class Main {
 
     public static void main(String[] args) {
-
+        int[] ints = sortedSquaredArray2(new int[]{-10, -5, 0, 5, 10});
+        System.out.println(Arrays.toString(ints));
     }
 
     public static int[] twoNumberSum(int[] array, int targetSum) {
@@ -39,6 +42,70 @@ public class Main {
             }
         }
         return new int[]{};
+    }
+
+    public static boolean isValidSubsequence(List<Integer> array, List<Integer> sequence) {
+        int iterator = 0;
+        for (Integer integer : array) {
+            int number = sequence.get(iterator);
+            if (number == integer) {
+                iterator++;
+            }
+            if (iterator == sequence.size()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int[] sortedSquaredArray(int[] array) {
+        return IntStream.of(array).map(i -> i*i).sorted().toArray();
+    }
+
+    public static int[] sortedSquaredArray2(int[] array) {
+        int[] resultArray = new int[array.length];
+        int max = resultArray.length - 1;
+        int min = 0;
+        for (int i = max; i >= 0; i--) {
+            if (abs(array[max]) < abs(array[min])) {
+                resultArray[i] = array[min] * array[min];
+                min++;
+            } else {
+                resultArray[i] = array[max] * array[max];
+                max--;
+            }
+        }
+        resultArray[0] = array[min] * array[min];
+        return resultArray;
+    }
+
+    public String tournamentWinner(ArrayList<ArrayList<String>> competitions, ArrayList<Integer> results) {
+        Map<String, Integer> teamToWins = new HashMap<>();
+        for (int i = 0; i < competitions.size(); i++) {
+            var competition = competitions.get(i);
+            var homeTeam = competition.get(0);
+            var awayTeam = competition.get(1);
+            var result = results.get(i);
+            if (result == 0) {
+                addPoints(teamToWins, awayTeam);
+            } else {
+                addPoints(teamToWins, homeTeam);
+            }
+        }
+        Optional<Map.Entry<String, Integer>> winningTeam = teamToWins.entrySet()
+                .stream()
+                .max(Comparator.comparingInt(Map.Entry::getValue));
+        return winningTeam.isPresent() ? winningTeam.get().getKey() : "Empty";
+    }
+
+    private void addPoints(Map<String, Integer> teamToWins, String team) {
+        var points = teamToWins.get(team);
+        if (points == null) {
+            teamToWins.put(team, 1);
+        } else {
+            points++;
+            teamToWins.put(team, points);
+        }
     }
 
 }
